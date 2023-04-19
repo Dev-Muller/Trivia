9f77277b6bd850a60445926d6b202a1106791c85
guipleit <112783039+guipleit@users.noreply.github.com>
2023-04-19 19:30:06 -0300

    feat: funcionalidade de logoff caso token estiver incorreto

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { fetchQuestions } from '../services/fetchApi';
 
 class Game extends Component {
   render() {
+    const { results } = JSON.parse(localStorage.getItem('results'));
+    console.log(results);
+
     return (
       <div />
     );
   }
 }
+
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Game;
