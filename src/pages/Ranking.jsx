c6a34cade72fd61c38473d126a302a5c2e586624
guipleit <112783039+guipleit@users.noreply.github.com>
2023-04-21 20:56:49 -0300

    feat: req 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <div data-testid="ranking-title">Ranking</div>
+      <div>
+        <div data-testid="ranking-title">Ranking</div>
+        <button
+          onClick={ () => history.push('/') }
+          data-testid="btn-go-home"
+        >
+          Play again
+        </button>
+      </div>
+
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Ranking;
